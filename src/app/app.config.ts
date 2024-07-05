c54092c0b528636dc1df924b19a84b0148e73898
Aditya Kulkarni <adityaabk165@gmail.com>
2024-07-05 14:43:32 +0530

    implemented counter with states

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { ReducerManager, provideState, provideStore } from '@ngrx/store';
+import { CounterReducer } from './states/counter/counter.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideStore(),
+  provideState({name:'counter', reducer:CounterReducer})
+]
 };
